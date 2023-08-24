e10c87ab0c3e79240e94427a9b097d15ffaa247b
Phil Liao <cheweiphil@gmail.com>
2023-08-24 10:41:16 -0500

    2.0

--- Ｍac/main.py
+++ Ｍac/main.py
@@ -1,6 +1,12 @@
 import pygame
 import time
 
-with open("ui.txt", "r") as file:
-    a = file.readline(1)
-    print(a)
+with open("/Users/philliao/Documents/Eye-Saver/Ｍac/setup.txt", "r") as file:
+    pass
+WIDTH, HEIGHT = 500, 300
+    
+    
+
+pygame.init()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Eye Saver")
