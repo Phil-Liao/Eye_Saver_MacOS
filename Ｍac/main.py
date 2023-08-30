19e01fc4f929d0d1be9529f484f2a0f094f66fff
Phil Liao <cheweiphil@gmail.com>
2023-08-30 08:35:16 -0500

    2

--- Ｍac/main.py
+++ Ｍac/main.py
@@ -1,12 +1,28 @@
 import pygame
 import time
 
-with open("/Users/philliao/Documents/Eye-Saver/Ｍac/setup.txt", "r") as file:
-    pass
-WIDTH, HEIGHT = 500, 300
-    
-    
-
 pygame.init()
+
+with open("/Users/philliao/Documents/Eye-Saver/Ｍac/setup.txt", "r") as file:
+    setup_info = file.readlines()
+    setup_info[0] = int(setup_info[0])
+    setup_info[1] = int(setup_info[1])
+
+
+WIDTH, HEIGHT = setup_info[0], setup_info[1]
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Eye Saver")
+pygame.display.set_caption(setup_info[2])
+
+
+
+
+
+
+FPS = 60
+while True:
+    pygame.time.Clock().tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        
+        
