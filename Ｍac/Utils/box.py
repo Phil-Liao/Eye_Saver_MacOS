c3ecfdea872d2ffa9d308309d47b30e7a59965f0
Phil Liao <cheweiphil@gmail.com>
2023-09-02 16:48:41 -0500

    5

--- Ｍac/Utils/box.py
+++ Ｍac/Utils/box.py
@@ -1,6 +1,6 @@
 import pygame
 class box:
-    def __init__(self, WIN, WIN_WIDTH, WIN_HEIGHT, x, y, width, height, passive_color, active_color=None, button=False):
+    def __init__(self, WIN, WIN_WIDTH, WIN_HEIGHT, x, y, width, height, passive_color, active_color=None, is_button=False, is_displaying_text=False):
         self.WIN = WIN
         self.WIN_WIDTH = WIN_WIDTH
         self.WIN_HEIGHT = WIN_HEIGHT
@@ -10,12 +10,23 @@ class box:
         self.height = WIN_HEIGHT*height
         self.passive_color = passive_color
         self.active_color = active_color
-        self.button = button
+        self.is_button = is_button
+        self.is_displaying_text = is_displaying_text
+    def is_display_text(self, font, text, text_color, text_x, text_y):
+        self.is_displaying_text = True
+        if not(isinstance(text, str)):
+            raise Exception(f"TypeError: expected str instance")
+        self.text = font.render(text, True, text_color)
+        self.text_rect = text.get_rect()
+        self.text_rect.center = (text_x, text_y)
+    def draw_text(self):
+        self.WIN.blit(self.text, self.text_rect)
     def draw_box(self, click=False):
-        if click and self.button:
+        if click and self.is_button:
             pygame.draw.rect(self.WIN, self.active_color, (self.x, self.y, self.width, self.height))
-        elif click and (self.button == False):
-            raise Exception("Error, class object not button")
+        elif click and (self.is_button == False):
+            raise Exception("Error: class object not button")
         else:
             pygame.draw.rect(self.WIN, self.passive_color, (self.x, self.y, self.width, self.height))
-    
+        if self.is_displaying_text:
+            draw_text()
--- Ｍac/main.py
+++ Ｍac/main.py
@@ -8,8 +8,7 @@ pygame.init()
 with open("/Users/philliao/Documents/Eye-Saver/Ｍac/setup.json") as file:
     setup_info = json.load(file)
 
-WIDTH, HEIGHT = setup_info["WIDTH"], setup_info["HEIGHT"]
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((setup_info["WIDTH"], setup_info["HEIGHT"]))
 pygame.display.set_caption(setup_info["SCREEN_CAPTION"])
 
 
@@ -22,19 +21,18 @@ def draw_background(WIN, BG_COLOR, WIDTH, HEIGHT):
 passive_color = Utils.COLORS[setup_info["PASSIVE_COLOR"]]
 active_color = Utils.COLORS[setup_info["ACTIVE_COLOR"]]
 
-control_box = Utils.box(WIN, WIDTH, HEIGHT, 0.025, 0.66, 0.45, 0.2, passive_color, active_color, True)
-input_box = Utils.box(WIN, WIDTH, HEIGHT, 0.525, 0.66, 0.45, 0.2, passive_color)
+control_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["CONTROL_BOX_X"], setup_info["CONTROL_BOX_Y"], setup_info["CONTROL_BOX_WIDTH"], setup_info["CONTROL_BOX_HEIGHT"], passive_color, active_color, True)
+input_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["INPUT_BOX_X"], setup_info["INPUT_BOX_Y"], setup_info["INPUT_BOX_WIDTH"], setup_info["INPUT_BOX_HEIGHT"], passive_color)
 
 
 
-FPS = 60
 while True:
-    pygame.time.Clock().tick(FPS)
+    pygame.time.Clock().tick(setup_info["FPS"])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             break
-    draw_background(WIN, Utils.colors.COLORS[setup_info["BG_COLOR"]], WIDTH, HEIGHT)
+    draw_background(WIN, Utils.colors.COLORS[setup_info["BG_COLOR"]], setup_info["WIDTH"], setup_info["HEIGHT"])
     control_box.draw_box(False)
     input_box.draw_box(False)
     pygame.display.update()
