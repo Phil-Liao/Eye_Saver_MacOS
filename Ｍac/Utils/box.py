f53d058af9334eac1ea9d7263659fef3aab93963
Phil Liao <cheweiphil@gmail.com>
2023-09-10 12:16:11 -0500

    Added interactive buttons

--- Ｍac/Utils/box.py
+++ Ｍac/Utils/box.py
@@ -1,6 +1,6 @@
 import pygame
 class box:
-    def __init__(self, WIN, WIN_WIDTH, WIN_HEIGHT, x, y, width, height, passive_color, is_a_button=False, is_displaying_text=False):
+    def __init__(self, WIN, WIN_WIDTH, WIN_HEIGHT, x, y, width, height, transparent=False, passive_color=(), border=False, border_width=0, border_radius=0, border_color=None):
         self.WIN = WIN
         self.WIN_WIDTH = WIN_WIDTH
         self.WIN_HEIGHT = WIN_HEIGHT
@@ -8,27 +8,69 @@ class box:
         self.y = WIN_HEIGHT*y
         self.width = WIN_WIDTH*width
         self.height = WIN_HEIGHT*height
-        self.passive_color = passive_color
-        self.is_a_button = is_a_button
-        self.is_displaying_text = is_displaying_text
-    def is_display_text(self, font, text, text_color, text_x, text_y):
-        self.is_displaying_text = True
+        self.transparent = transparent
+        if not(self.transparent):
+            self.passive_color = passive_color
+        self.button = False
+        self.display_text = False
+        self.border = border
+        if self.border:
+            self.border_width = border_width
+            self.border_radius = border_radius
+            if border_color == -1:
+                self.border_color = passive_color
+            else:
+                self.border_color = border_color
+    def is_display_text(self, font, text, text_passive_color, text_x, text_y, text_active_color = None):
+        self.display_text = True
         if not(isinstance(text, str)):
             raise Exception(f"TypeError: expected str instance")
-        self.text = font.render(text, True, text_color)
-        self.text_rect = text.get_rect()
-        self.text_rect.center = (text_x, text_y)
+        self.text_passive = font.render(text, True, text_passive_color)
+        self.text_rect_passive = self.text_passive.get_rect()
+        self.text_rect_passive.center = ((self.x+(self.width*text_x)), (self.y+(self.height*text_y)))
+        if text_active_color != None:
+            if self.button:
+                self.text_active = font.render(text, True, text_active_color)
+                self.text_rect_active = self.text_active.get_rect()
+                self.text_rect_active.center = ((self.x+(self.width*text_x)), (self.y+(self.height*text_y)))
+            else:
+                raise Exception("box.error box object not set as button")
+    
     def is_button(self, active_color):
-        self.is_a_button = True
+        self.button = True
         self.active_color = active_color
-    def draw_text(self):
-        self.WIN.blit(self.text, self.text_rect)
-    def draw_box(self, click=False):
-        if click and self.is_button:
-            pygame.draw.rect(self.WIN, self.active_color, (self.x, self.y, self.width, self.height))
-        elif click and (self.is_button == False):
-            raise Exception("Error: class object not button")
+    
+    
+    def is_input(self):
+        pass
+    def input(self):
+        pass
+    
+    def clicked(self, coordinates):
+        x, y = coordinates[0], coordinates[1]
+        x_boundry = self.x <= x <= self.x + self.width
+        y_boundry = self.y <= y <= self.y + self.height
+        if x_boundry and y_boundry:
+            return True
         else:
-            pygame.draw.rect(self.WIN, self.passive_color, (self.x, self.y, self.width, self.height))
-        if self.is_displaying_text:
-            draw_text()
+            return False
+    
+    def draw_text(self, click):
+        if not(click):
+            self.WIN.blit(self.text_passive, self.text_rect_passive)
+        else:
+            self.WIN.blit(self.text_active, self.text_rect_active)
+    def draw_box(self, click=False):
+        if not(self.transparent) and click and self.border and self.button:
+            pygame.draw.rect(self.WIN, self.active_color, ((self.x + self.border_width), (self.y + self.border_width), (self.width - 2*self.border_width), (self.height - 2*self.border_width)))
+            pygame.draw.rect(self.WIN, self.active_color, (self.x, self.y, self.width, self.height), self.border_width, self.border_radius)
+        elif self.transparent and click and self.border and self.button:
+            pygame.draw.rect(self.WIN, self.active_color, (self.x, self.y, self.width, self.height), self.border_width, self.border_radius)
+        elif not(self.transparent) and not(click) and self.border:
+            pygame.draw.rect(self.WIN, self.passive_color, ((self.x + self.border_width), (self.y + self.border_width), (self.width - 2*self.border_width), (self.height - 2*self.border_width)))
+            pygame.draw.rect(self.WIN, self.passive_color, (self.x, self.y, self.width, self.height), self.border_width, self.border_radius)
+        elif self.transparent and not(click):
+            pygame.draw.rect(self.WIN, self.border_color, (self.x, self.y, self.width, self.height), self.border_width, self.border_radius)
+
+        if self.display_text:
+            self.draw_text(click)
--- Ｍac/main.py
+++ Ｍac/main.py
@@ -30,15 +30,27 @@ def draw_background(WIN, BG_COLOR, WIDTH, HEIGHT, font=None, text=None, text_col
         text_rect.center = (text_x,text_y)
     WIN.blit(text, text_rect)
 
-passive_color = Utils.COLORS[setup_info["PASSIVE_COLOR"]]
-active_color = Utils.COLORS[setup_info["ACTIVE_COLOR"]]
 
-control_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["CONTROL_BOX_X"], setup_info["CONTROL_BOX_Y"], setup_info["CONTROL_BOX_WIDTH"], setup_info["CONTROL_BOX_HEIGHT"], passive_color, True)
-input_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["INPUT_BOX_X"], setup_info["INPUT_BOX_Y"], setup_info["INPUT_BOX_WIDTH"], setup_info["INPUT_BOX_HEIGHT"], passive_color)
+control_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["CONTROL_BOX_X"], setup_info["CONTROL_BOX_Y"], setup_info["CONTROL_BOX_WIDTH"], setup_info["CONTROL_BOX_HEIGHT"], True, None, 
+                        True, setup_info["CONTROL_BOX_BORDER_WIDTH"], setup_info["CONTROL_BOX_BORDER_RADIUS"], Utils.COLORS[setup_info["CONTROL_BOX_BORDER_COLOR"]])
+control_box.is_button(Utils.COLORS[setup_info["ACTIVE_COLOR"]])
+control_box_font = set_font("Segoe-UI", 24)
+control_box_text = setup_info["CONTROL_BOX_STARTUP_TEXT"]
+control_box.is_display_text(control_box_font, control_box_text, Utils.COLORS[setup_info["TEXT_PASSIVE_COLOR"]], setup_info["CONTROL_BOX_TEXT_X"], setup_info["CONTROL_BOX_TEXT_Y"],
+                            Utils.COLORS[setup_info["TEXT_ACTIVE_COLOR"]])
+
+
+
+
+input_box = Utils.box(WIN, setup_info["WIDTH"], setup_info["HEIGHT"], setup_info["INPUT_BOX_X"], setup_info["INPUT_BOX_Y"], setup_info["INPUT_BOX_WIDTH"], setup_info["INPUT_BOX_HEIGHT"], False, Utils.COLORS[setup_info["PASSIVE_COLOR"]], True,
+                      setup_info["INPUT_BOX_BORDER_WIDTH"], setup_info["INPUT_BOX_BORDER_RADIUS"], -1)
+input_box.is_button(Utils.COLORS[setup_info["ACTIVE_COLOR"]])
 
 
 
 while True:
+    control_box_click = False
+    input_box_click = False
     pygame.time.Clock().tick(setup_info["FPS"])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,7 +58,8 @@ while True:
             break
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            print(mouse_pos)
+            control_box_click = control_box.clicked(mouse_pos)
+            input_box_click = input_box.clicked(mouse_pos)
             
 
 
@@ -56,8 +69,8 @@ while True:
     bg_font = set_font(setup_info["BG_TEXT_TYPE"], setup_info["BG_TEXT_SIZE"])
     
     main_text = setup_info["BG_STARTUP_TEXT"]
-    draw_background(WIN, Utils.colors.COLORS[setup_info["BG_COLOR"]], setup_info["WIDTH"], setup_info["HEIGHT"], bg_font, main_text, Utils.COLORS[setup_info["TEXT_COLOR"]])
+    draw_background(WIN, Utils.colors.COLORS[setup_info["BG_COLOR"]], setup_info["WIDTH"], setup_info["HEIGHT"], bg_font, main_text, Utils.COLORS[setup_info["TEXT_PASSIVE_COLOR"]])
     
-    control_box.draw_box(False)
-    input_box.draw_box(False)
+    control_box.draw_box(control_box_click)
+    input_box.draw_box(input_box_click)
     pygame.display.update()
